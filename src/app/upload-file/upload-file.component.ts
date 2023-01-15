3a15a1467b0a63ce36f831f5d1da1adcff333d87
Abhihsek Pankar <abhishek.pankar11@gmail.com>
2023-01-15 18:11:09 +0530

    file upload page done

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-upload-file',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload-file.component.css']
 })
 export class UploadFileComponent {
+  file?: File;
+  fileResponse? : any;
 
+  selectFile(target: (EventTarget | null) | HTMLInputElement) {
+    this.fileResponse = null;
+    if (target instanceof HTMLInputElement && target.files) {
+      this.file = target.files[0];
+    } else {
+      alert('Something went wrong');
+    }
+  }
+
+  async upload() {
+    try {
+      const formdata = new FormData();
+      formdata.append('image', this.file!);
+      const response = await axios.post('http://localhost:4000/files/upload', formdata);
+      alert(response.data.message);
+      this.fileResponse = response.data.data;
+      this.file = undefined;
+    } catch (error: any) {
+      alert(error.response.data.message);
+    }
+  }
 }
